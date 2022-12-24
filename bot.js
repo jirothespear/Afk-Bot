0640094b4b893b99070138b59cadfb0e2b23a334
jirothespear <89335902+jirothespear@users.noreply.github.com>
2022-12-24 21:06:22 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const app = require('/.app');
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
-server.listen(port, hostname:() => {
+server.listen(port, hostname:()) => {
    console.log('Server started on port ${port}');
                
     }};

736c7c399719be03e14c1561f9e9cfd1f44c6e4a
jirothespear <89335902+jirothespear@users.noreply.github.com>
2022-12-27 08:50:34 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,9 +13,7 @@ app.get('/', (req, res) => {
   res.send('Bot Is Ready')
 });
 
-app.listen(3000, () => {
-  console.log('server started');
-});
+
 
 function createBot() {
    const bot = mineflayer.createBot({
@@ -125,5 +123,9 @@ function createBot() {
       console.log(`\x1b[31m[ERROR] ${err.message}`, '\x1b[0m')
    );
 }
+
+app.listen(3000, () => {
+  console.log('server started');
+});
 
 createBot();
